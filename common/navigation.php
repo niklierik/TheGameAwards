1434081f0b2e8def5bbe51e972d6024659a80321
Erik <erik200203@gmail.com>
2022-04-04 19:19:04 +0200

    Erik

--- common/navigation.php
+++ common/navigation.php
@@ -16,6 +16,7 @@ foreach ($year as $y => $g) {
 echo "<hr>
     <button id=\"showeditor\">Szerkesztői<br>Opciók</button>";
 navBtn("login.php", "Belépés mint<br>Szerkesztő", " editor");
+navBtn("register.php", "Regisztrálás mint<br>Szerkesztő", " editor");
 echo "<hr>";
 echo " <a class=\"navelement\" href=\"#title_div\"><button>Vissza az oldal elejére</button></a>
     <div style = \"padding-bottom: 90px\"></div>
---
+++ register.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<?php
+/**
+ *  Szóval tök valós, hogy van egy regisztrációs menüd ahol mindenféle jött ment fel tud regelni szerkesztőként c:
+ */
+
+
+
+?>
+<html lang="en">
+
+<head>
+    <title>
+        Regisztráció
+    </title>
+    <?php
+        include "common/imports.php";
+    ?>
+</head>
+<body>
+<!--
+    Fejléc
+-->
+
+<?php
+include "common/header.php";
+?>
+<!--
+    Szöveges tartalom
+-->
+<div class="main_main_div">
+    <main class="textcontent main first last">
+        <form action="register.php">
+
+        </form>
+    </main>
+
+    <?php
+    include "common/navigation.php";
+    ?>
+</div>
+
+<?php
+include "common/footer.php";
+?>
+
+</body>
+
+</html>
